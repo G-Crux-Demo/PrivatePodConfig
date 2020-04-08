505338b8fa5f7b0fa40688faf5dc9a833b595da4
Lfm <liufm@xiaoji.com>
2020-04-08 17:09:16 +0800

    First commit!

---
+++ templates/upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VersionString=`grep -E 's.version.*=' __ProjectName__.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+
+NewVersionNumber=$(($VersionNumber + 1))
+LineNumber=`grep -nE 's.version.*=' __ProjectName__.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" __ProjectName__.podspec
+
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+
+git add .
+git commit -am ${NewVersionNumber}
+git tag ${NewVersionNumber}
+git push origin master --tags
+#此处的PrivatePods修改为你在github上创建的（私有pod）repo名
+pod repo push PrivatePods __ProjectName__.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
+
